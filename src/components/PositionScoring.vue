c9a4a58403c9e0eb52d85402d07c9184087ed3f6
ItisFeathers <williamgeorgeburgess@gmail.com>
2025-03-23 21:55:19 +0000

    adds a review pane

---
+++ src/components/PositionScoring.vue
@@ -0,0 +1,67 @@
+<template>
+    <v-card
+    >
+        <v-card-title>AAAAAAA</v-card-title>
+        <v-progress-circular
+            :model-value="score * 100"
+
+            :size="100"
+            :width="15"
+            color="red"
+            >
+            {{ Math.round(score * 100) }}%
+        </v-progress-circular>
+        <v-card-actions>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { Repertoire } from "./dom/repertoire";
+import { ResultsSummary } from "./dom/testResult";
+import { getPlayerToMove } from "./dom/fenUtils";
+  
+const props = defineProps({
+    repertoire: {
+        type: Repertoire,
+        required: true,
+    },
+    resultsSummary: {
+        type: ResultsSummary,
+        required: true,
+    },
+    currentPosition: {
+        type: String,
+        required: true
+    },
+    player: {
+        type: String,
+        required: true
+    }
+});
+
+const score = computed(() => {
+    console.log("recompute")
+    // if (getPlayerToMove(props.currentPosition) == props.player) {
+    return props.resultsSummary.getCompleteness(props.currentPosition, props.player, props.repertoire, 4);
+});
+
+
+
+  
+  
+</script>
+  
+  <style lang="css" scoped>
+  .selected {
+    background-color: #66ff66;
+  }
+  .alternative {
+    background-color: #FFff66;
+  }
+  .option {
+    background-color: #6666ff;
+  }
+  </style>
+  
